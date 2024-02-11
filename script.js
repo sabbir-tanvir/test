dceed9e0c27a318ee42faff55f2e44f7b6996161
Sabbir Tanvir <106466798+sabbir-tanvir@users.noreply.github.com>
2024-02-11 11:03:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+const form = document.querySelector("form");
+
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
@@ -20,21 +29,21 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height){
+        if (top >= offset && top < offset + height) {
 
             navLinks.forEach(links => {
                 links.classList.remove('active');
-        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
 
             sec.classList.add('show-animate');
-            
+
         }
         else {
             sec.classList.remove('show-animate');
         }
     });
-    
+
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
@@ -44,6 +53,90 @@ window.onscroll = () => {
     let footer = document.querySelector('footer');
 
     footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
-    
+
 
 }
+
+
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br>
+  Email: ${email.value}<br>
+  Phone Number: ${phone.value}<br>
+  Subject: ${subject.value}<br>
+  Message: ${message.value}`;
+
+    Email.send({
+        SecureToken: "8b5e044a-0840-4b55-b91c-f21f2880a8e2",
+        To: 'sabbirmahmudtanvir@gmail',
+        From: "sabbirmahmudtanvir@gmail",
+        Subject: subject.value,
+        Body: bodyMessage
+    }).then(
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Good job!",
+                    text: "You clicked the button!",
+                    icon: "success"
+                  });
+            }
+        }
+    );
+}
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+    for (const item of items ){
+        if (item.value == ""){
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != ""){
+            checkEmail();
+        }
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        });
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+        })
+    }
+}
+ function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+    }
+    else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+ }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    checkInputs()
+
+    if (!fullName.classList.contains("error") && 
+    !email.classList.contains("error") &&
+    !phone.classList.contains("error") &&
+    !subject.classList.contains("error") &&
+    !message.classList.contains("error")) {
+        sendEmail();
+
+        form.reset();
+        return false;
+    }
+    
+});
